2db22bdc428d0258841694714c6c05f5ed4eee11
Linda Woo <lindawoo@lindas-mbp.gateway.sonic.net>
2020-07-25 23:43:19 -0700

    description and about me

--- src/Landing.js
+++ src/Landing.js
@@ -2,60 +2,78 @@ import React, {Component} from "react";
 import classes from "./Landing.module.css";
 import Linda from './images/lindaWooHeadShot.jpeg';
 import Rezoom from './images/rezoom.jpeg';
+import Phinney from './images/Phinney.jpeg';
+import NavBtn from './images/navBtn.png';
+import Bean from './images/Bean.png';
+import Seashell from './images/seashell.png';
+
 
 class Landing extends Component {
     state={
-        skillsRef : React.createRef()
+        skillsRef : React.createRef(),
+        projectsRef: React.createRef()
     }
-
     render(){
-
         let place = 0;
         window.addEventListener("scroll", (e)=>{
             if(this.state.skillsRef.current){
             place = window.pageYOffset;
-            this.state.skillsRef.current.style.transform = `translate(0%,${.7*place}px)`;
+            this.state.skillsRef.current.style.transform = `translate(0%,${.2*place}px)`;
+            this.state.projectsRef.current.style.transform = `translate(0%,${.4*place}px)`;
             }
         })
-        
-
         return(
             <div className = {classes.Back}>
                 <div className = {classes.Landing}>
-                <div className = {classes.Heading}>
-                    <p>Linda Woo</p>
-                    <span>Frontend Software Developer</span>
-                    <img className = {classes.Linda} src = {Linda} />
-                </div>
-                <div className = {classes.Projects}>
-                    <img src = {Rezoom} />
-                </div>
-                <div className = {classes.SkillsContainer} ref = {this.state.skillsRef}>
-                    <div className = {[classes.Skills, classes.JavaScript].join(" ")}>
-                        JavaScript
+                    <div className = {classes.Navbar}>
+                        <a href = '#AboutMe'> About Me</a> 
+                        <a href = "#Projects"> My Projects</a>
+                        <a> Contact Me</a>
                     </div>
-                    <div className = {[classes.Skills,classes.React].join(" ")}>
-                        ReactJS
+                    <div className = {classes.Heading}>
+                        <img className = {classes.Linda} src = {Linda} />
+                        
+                        <div className = {classes.Text}>
+                            <p className = {classes.Name}>Linda Woo</p>
+                            <p className = {classes.Title}>Frontend Software Developer</p>
+                        </div>
                     </div>
-                    <div className = {[classes.Skills, classes.Html].join(" ")}>
-                        HTML
+                    <div id = "AboutMe" className = {classes.AboutMe}>
+                        <div className = {classes.SkillsContainer} ref = {this.state.skillsRef}>
+                            <div className = {[classes.Skills, classes.JavaScript].join(" ")}>
+                                JavaScript
+                            </div>
+                            <div className = {[classes.Skills,classes.React].join(" ")}>
+                                ReactJS
+                            </div>
+                            <div className = {[classes.Skills, classes.Html].join(" ")}>
+                                HTML
+                            </div>
+                            <div className = {[classes.Skills, classes.Css].join(" ")}>
+                                CSS
+                            </div>
+                            <div className = {[classes.Skills, classes.JQuery].join(" ")}>
+                                JQuery
+                            </div>
+                        </div>
+                        <div className = {classes.Description}>
+                            I am an entry-level frontend web developer with over 20 years of professional work experience.
+                        </div>
                     </div>
-                    <div className = {[classes.Skills, classes.Css].join(" ")}>
-                        CSS
-                    </div>
-                    <div className = {[classes.Skills, classes.JQuery].join(" ")}>
-                        JQuery
+                    <div id = "Projects" className = {classes.Projects} ref = {this.state.projectsRef}>
+                        <div className = {classes.Project}>
+                            <img src = {Rezoom} /> 
+                            <p> Resume site for first-time job seekers</p>
+                        </div>
+                        <div className = {classes.Project}>
+                            <img src = {Phinney} /> 
+                            <p> Seattle Optometry Shop</p>
+                        </div>
                     </div>
                 </div>
             </div>
-            </div>
-            
         )
-
-
     }
-
-
 }
 
 export default Landing;
