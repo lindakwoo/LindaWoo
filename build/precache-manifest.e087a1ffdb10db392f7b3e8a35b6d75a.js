14b6ad9435ba2aea4fc040f0dfd5e11928c5ace0
Linda Woo <lindakwoo@gmail.com>
2020-09-24 14:39:35 -0700

    final commit

---
+++ build/precache-manifest.e087a1ffdb10db392f7b3e8a35b6d75a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea6f7eccf446f55250164c4e8265f5ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63e74422c5e88bc25e4f",
+    "url": "/static/css/main.a9df11b5.chunk.css"
+  },
+  {
+    "revision": "57994c3ba888f0e3f0f6",
+    "url": "/static/js/2.b92184a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b92184a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63e74422c5e88bc25e4f",
+    "url": "/static/js/main.4fe14626.chunk.js"
+  },
+  {
+    "revision": "8d8ce16756630a13206b",
+    "url": "/static/js/runtime-main.8de8249c.js"
+  },
+  {
+    "revision": "ffb0ccc3a4c394cfc36e5b530b2f14ca",
+    "url": "/static/media/Phinney2.ffb0ccc3.jpeg"
+  },
+  {
+    "revision": "dab0c484d8141edc55539b4609f299b1",
+    "url": "/static/media/SF.dab0c484.jpg"
+  },
+  {
+    "revision": "e39a9532aee98317091ae61df613dd20",
+    "url": "/static/media/beige2.e39a9532.png"
+  },
+  {
+    "revision": "001ebe3045c3d42b774ea9241ff61595",
+    "url": "/static/media/brown.001ebe30.png"
+  },
+  {
+    "revision": "8f5b9e7c3160e1b84d7f5ee1c7251f01",
+    "url": "/static/media/circles.8f5b9e7c.jpg"
+  },
+  {
+    "revision": "cc5150b69e24dcaa2d27e932981bc304",
+    "url": "/static/media/green.cc5150b6.png"
+  },
+  {
+    "revision": "98e012527bfd3d71ffa27b02d676358f",
+    "url": "/static/media/lindaWooHeadShot.98e01252.jpeg"
+  },
+  {
+    "revision": "6844403d8c3c637426b52bf2248ed0d5",
+    "url": "/static/media/linkedin.6844403d.png"
+  },
+  {
+    "revision": "d2ccef64a0b8db0af49df3918d4684f0",
+    "url": "/static/media/pink2.d2ccef64.png"
+  },
+  {
+    "revision": "5e88e3dd7758834c4d8aada157720ff1",
+    "url": "/static/media/rezoom.5e88e3dd.jpeg"
+  },
+  {
+    "revision": "7908d0c0cc46b4f39e546a39e66b983e",
+    "url": "/static/media/seashell.7908d0c0.png"
+  },
+  {
+    "revision": "2e6f9c96be976374910e55536e64be9c",
+    "url": "/static/media/tan.2e6f9c96.png"
+  }
+]);
--- src/Landing.js
+++ src/Landing.js
@@ -2,34 +2,44 @@ import React, {Component} from "react";
 import classes from "./Landing.module.css";
 import Linda from './images/lindaWooHeadShot.jpeg';
 import Rezoom from './images/rezoom.jpeg';
-import Phinney from './images/Phinney.jpeg';
-import NavBtn from './images/navBtn.png';
-import Bean from './images/Bean.png';
+import Phinney from './images/Phinney2.jpeg';
 import Seashell from './images/seashell.png';
-
+import Airplane from './images/email.png';
+import Linkedin from './images/linkedin.png';
 
 class Landing extends Component {
     state={
         skillsRef : React.createRef(),
-        projectsRef: React.createRef()
+        projectsRef: React.createRef(),
+        landingRef: React.createRef(),
+        descriptionRef:React.createRef(),
+        contactRef: React.createRef()
     }
     render(){
         let place = 0;
         window.addEventListener("scroll", (e)=>{
-            if(this.state.skillsRef.current){
-            place = window.pageYOffset;
-            this.state.skillsRef.current.style.transform = `translate(0%,${.2*place}px)`;
-            this.state.projectsRef.current.style.transform = `translate(0%,${.4*place}px)`;
+            if(this.state.skillsRef.current&&this.state.skillsRef.current&&this.state.contactRef.current){
+                place = window.pageYOffset;
+                this.state.skillsRef.current.style.transform = `translate(0%,${.2*place}px)`;
+                this.state.projectsRef.current.style.transform = `translate(0%,${.33*place}px)`;
+                this.state.landingRef.current.style.top = `${-place}px`;
+                this.state.contactRef.current.style.transform = `translate(0%,${.45*place}px)`;
+                console.log(place)
+                // if(place>794){
+                //     this.state.landingRef.current.style.position="relative";
+                    // this.state.descriptionRef.current.sytle.color="red";
+                // }
+                
             }
         })
         return(
             <div className = {classes.Back}>
-                <div className = {classes.Landing}>
-                    <div className = {classes.Navbar}>
-                        <a href = '#AboutMe'> About Me</a> 
-                        <a href = "#Projects"> My Projects</a>
-                        <a> Contact Me</a>
+                <div className = {classes.Navbar}>
+                        <a href = '#AboutMePlaceholder'> About Me</a> 
+                        <a href = "#ProjectsPlaceholder"> My Projects</a>
+                        <a href = "#ContactPlaceholder"> Contact Me</a>
                     </div>
+                <div className = {classes.Landing} ref = {this.state.landingRef}>
                     <div className = {classes.Heading}>
                         <img className = {classes.Linda} src = {Linda} />
                         
@@ -38,7 +48,11 @@ class Landing extends Component {
                             <p className = {classes.Title}>Frontend Software Developer</p>
                         </div>
                     </div>
+
+                </div>
+                <div className = {classes.LandingBack}></div>
                     <div id = "AboutMe" className = {classes.AboutMe}>
+                        <div id = "AboutMePlaceholder" className ={classes.AboutMePlaceholder}>about me placeholder</div>
                         <div className = {classes.SkillsContainer} ref = {this.state.skillsRef}>
                             <div className = {[classes.Skills, classes.JavaScript].join(" ")}>
                                 JavaScript
@@ -56,21 +70,39 @@ class Landing extends Component {
                                 JQuery
                             </div>
                         </div>
-                        <div className = {classes.Description}>
-                            I am an entry-level frontend web developer with over 20 years of professional work experience.
+                        <div className = {classes.Description}ref = {this.state.descriptionRef}>
+                            I am an entry-level frontend web developer in San Francisco with over 10 years of professional work experience.
                         </div>
                     </div>
+                    <div id = "ProjectsPlaceholder" className ={classes.ProjectsPlaceholder}>projects placeholder</div>
                     <div id = "Projects" className = {classes.Projects} ref = {this.state.projectsRef}>
-                        <div className = {classes.Project}>
-                            <img src = {Rezoom} /> 
-                            <p> Resume site for first-time job seekers</p>
-                        </div>
-                        <div className = {classes.Project}>
-                            <img src = {Phinney} /> 
-                            <p> Seattle Optometry Shop</p>
+                        <p className = {classes.ProjectsText}>Projects I'm currently working on</p>
+                        <div className = {classes.ProjectsContainer}>
+                            <div className = {classes.Project}>
+                                <div className = {classes.LinkContainer}>
+                                
+                                    <a className = {classes.RezoomLink} href = "https://rezoom3.web.app/"> <img className = {classes.RezoomPic} src = {Rezoom} /> </a>
+                                </div>
+                                <p> Resume site for first-time job seekers</p>
+                            </div>
+                            <div className = {classes.Project}>
+                            <div className = {classes.LinkContainer}>
+                                
+                                <a className = {classes.PhinneyLink} href = "https://functions-32143.web.app/"> <img className = {classes.PhinneyPic} src = {Phinney} /> </a>
+                            </div>
+                                <p> Seattle Optometry Shop</p>
+                            </div>
                         </div>
                     </div>
-                </div>
+                    <div className = {classes.ContactContainer} >
+                        <div id = "ContactPlaceholder" className ={classes.ContactPlaceholder}>contact placeholder</div>
+
+                        <div className= {classes.Contact} ref = {this.state.contactRef}>
+                            <p className = {classes.Name}>Linda Woo</p>
+                            <p className = {classes.Link}><a href = "mailto:lindakwoo@gmail.com"><img src = {Airplane}/>email</a></p>
+                            <p className = {classes.Link}><a href = "https://linkedin.com/in/lindakwoo"> <img src = {Linkedin}/>linkedin</a></p>
+                        </div>
+                    </div>
             </div>
         )
     }
